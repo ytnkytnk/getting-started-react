6b01adcb694aea6a8c4ab78db6a59d53c82058d4
Yoko Tanaka <yotanaka@indeed.com>
2024-06-13 15:21:09 +0900

    update for render assessment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // // hello alice block
+  // const name = "Alice";
+  // const element = <h1>Hello {name}!</h1>;
+
+  // // total purchase block
+  // const item = "apple";
+  // const price = 2;
+  // const count = 10;
+  // const element = (
+  //   <div>
+  //     <h1>Total Purchased:</h1>
+  //     <p>
+  //       Purchased {count} {item}s for ${price} each. The total is $
+  //       {count * price}.;
+  //     </p>
+  //   </div>
+  // );
+
+  // // url block
+  // const name = "Alice";
+  // const url = "http://www.thinkful.com/";
+  // const linkText = "Thinkful";
+  // const element = (
+  //   <p>
+  //     {" "}
+  //     Hey, {name}! Please visit <a href={url}>{linkText}</a>.
+  //   </p>
+  // );
+
+  // // multiple top level elements are not allowed, wrap by <div> or react fragment <>
+  // const element = (
+  //   <>
+  //     <h1>This is H1 element</h1>
+  //     <h2>This is H2 element</h2>
+  //   </>
+  // );
+
+  // closing tag is not necessary for some cases (img, input, link, br, hr)
+  const element = (
+    <>
+      <h2>Heading</h2>
+      <img src="./favicon.ico" alt="react icon" width="100px" />
+      <br />
+    </>
   );
+
+  return element;
 }
 
 export default App;
